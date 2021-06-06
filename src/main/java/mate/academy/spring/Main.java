6e36a9c26c4a481f4b07cafcd6743317b72a5700
dant0n <dmibuc@gmail.com>
2021-06-06 15:14:27 +0300

    Finish hw.

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,25 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        User firstUser = new User();
+        firstUser.setFirstName("User1 FirstName");
+
+        User secondUser = new User();
+        secondUser.setLastName("User2 LastName");
+
+        userService.add(firstUser);
+        userService.add(secondUser);
+
+        System.out.println(userService.listUsers());
+    }
 }
