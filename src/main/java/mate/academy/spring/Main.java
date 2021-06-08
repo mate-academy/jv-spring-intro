8dfddd7352bb924769c53b52b9f5a68ecb5aba88
Dalai <semenov.dalay@gmail.com>
2021-06-08 14:35:05 +0300

    done

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,20 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        User a = new User("a@gmail.com", "aaaa");
+        User b = new User("b@gmail.com", "bbbb");
+        UserService userService = context.getBean(UserService.class);
+        userService.add(a);
+        userService.add(b);
+        System.out.println(userService.listUsers());
+    }
 }
