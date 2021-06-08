f8bfc0abe4ecc51b8d2c76b7183efc5bba3d7996
Nikber5 <nikber198@gmail.com>
2021-06-08 09:56:56 +0300

    made environment final

--- src/main/java/mate/academy/spring/config/AppConfig.java
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -14,7 +14,7 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "mate.academy.spring")
 public class AppConfig {
-    private Environment environment;
+    private final Environment environment;
 
     public AppConfig(Environment environment) {
         this.environment = environment;
