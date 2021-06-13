25bcfdd36764a9033598c5d53f1c571e755e6a6e
fotomassimo <maksym.pavliuchenko@gmail.com>
2021-06-14 02:01:05 +0300

    initial commit

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,24 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User alice = new User();
+        alice.setEmail("alice@gmail.com");
+        alice.setPassword("1234");
+        User bob = new User();
+        bob.setEmail("bob@gmail.com");
+        bob.setPassword("bob1234");
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(alice);
+        userService.add(bob);
+        System.out.println(userService.listUsers());
+    }
 }
---
+++ src/main/java/mate/academy/spring/dao/UserDaoImpl.java
@@ -0,0 +1,54 @@
+package mate.academy.spring.dao;
+
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert to DB user: " + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> listUsers() {
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaQuery<User> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(User.class);
+            criteriaQuery.from(User.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get all users", e);
+        }
+    }
+}
