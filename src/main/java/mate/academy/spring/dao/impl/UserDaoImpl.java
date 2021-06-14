8cfc8d87bd4bd097eddf8e45bf996cf6b4227e74
VladislavTetar <luxeon010203@gmail.com>
2021-06-14 18:49:24 +0300

    first solution.

---
+++ src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
@@ -0,0 +1,54 @@
+package mate.academy.spring.dao.impl;
+
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private final SessionFactory factory;
+
+    @Autowired
+    public UserDaoImpl(SessionFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public void add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("An error occurred while "
+                    + "processing query to add new user = " + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> listUsers() {
+        try (Session session = factory.openSession()) {
+            CriteriaQuery<User> query =
+                    session.getCriteriaBuilder().createQuery(User.class);
+            query.from(User.class);
+            return session.createQuery(query).getResultList();
+        }
+    }
+}
