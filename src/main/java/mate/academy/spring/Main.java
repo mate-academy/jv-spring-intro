350cc038ce6f3e8242ae0ed34740f7383840a985
DanyloIT <krupnyk.danylo@gmail.com>
2021-06-14 15:47:37 +0300

    all done + build success

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,20 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        User danylo = new User();
+        danylo.setEmail("danylo@gmail.com");
+        danylo.setPassword("111");
+        UserService userService = context.getBean(UserService.class);
+        userService.add(danylo);
+        System.out.println(userService.listUsers());
+    }
 }
