a841106a0ac75438b38aa55ddfedb4e2bd4d50e3
Yevhenii Denysenko <iedenysenko@gmail.com>
2021-06-14 13:41:15 +0300

    add solution

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,28 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User ben = new User();
+        ben.setEmail("ben@gmail.com");
+        ben.setPassword("123456");
+        
+        User alex = new User();
+        alex.setEmail("alex@gmail.com");
+        alex.setPassword("654321");
+        
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                AppConfig.class);
+        
+        UserService userService = context.getBean(UserService.class);
+        userService.add(ben);
+        userService.add(alex);
+        for (User user : userService.listUsers()) {
+            System.out.println(user);
+        }
+    }
 }
