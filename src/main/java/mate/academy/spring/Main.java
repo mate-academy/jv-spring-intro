221b26c52d5c782b8d7e6d95f5cfa47c1f6fd242
Darina <to.darina.to>
2021-06-14 14:35:23 +0300

    all done

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,20 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.ApplicationContext;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setEmail("email@.com");
+        user.setPassword("somePassword");
+        AnnotationConfigApplicationContext context = new
+                AnnotationConfigApplicationContext(ApplicationContext.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(user);
+        userService.getAllUsers().forEach(System.out::println);
+
+    }
 }
---
+++ src/main/java/mate/academy/spring/service/imp/UserServiceImp.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service.imp;
+
+import java.util.List;
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImp implements UserService {
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+}
