cdac1392b33effe76cea927d19c4428ad9cb4a7f
Дмитро <dima4994@gmail.com>
2021-06-14 15:30:24 +0300

    There were created User class, DAO and Service layers

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,21 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        User chris = new User("Chris", 21, "visitor");
+        userService.add(chris);
+        User morgan = new User("Morgan", 23, "owner");
+        userService.add(morgan);
+
+        userService.listUsers().forEach(System.out::println);
+    }
 }
