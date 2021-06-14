ca9f2ad88c215e9cf58e74d17fbafa91408e4d6c
dvdovynets <vdovinec@gmail.com>
2021-06-14 16:11:35 +0300

    made suggested changes

--- src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-    @Autowired
     private final UserDao userDao;
 
+    @Autowired
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
     }
