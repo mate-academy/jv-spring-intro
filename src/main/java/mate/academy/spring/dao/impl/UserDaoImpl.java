ba8011de9d16192c5b6707f66532b047a77b5d2d
oleksandr-shulha <shulgalex@gmail.com>
2021-06-15 00:20:19 +0300

    working

--- src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
@@ -5,7 +5,6 @@ import javax.persistence.criteria.CriteriaQuery;
 import mate.academy.spring.dao.UserDao;
 import mate.academy.spring.exceptions.DataProcessingException;
 import mate.academy.spring.model.User;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -1,6 +1,5 @@
 package mate.academy.spring.model;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
