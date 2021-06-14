13726e2eb2c3377c22ad14dc01984eb89f3318e5
m.kislukhina <marharita.kislukhina@gmail.com>
2021-06-14 11:27:13 +0200

    done

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,26 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        User harryPotter = new User("Harry", "Potter");
+
+        User ronWeasley = new User("Ron", "Weasley");
+
+        User hermioneGranger = new User("hHermione", "Granger");
+
+        userService.add(harryPotter);
+        userService.add(ronWeasley);
+        userService.add(hermioneGranger);
+
+        userService.listUsers().forEach(System.out::println);
+    }
 }
