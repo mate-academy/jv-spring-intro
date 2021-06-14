71b066014e1238f50dd59327cbb9834e052dd5a4
Ksenia <ksenia.obertii@gmail.com>
2021-06-14 15:50:28 +0300

    added solution

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,22 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+    private static final UserService userService = context.getBean(UserService.class);
+
+    public static void main(String[] args) {
+        User userBob = new User("bob.bobchenko@gmail.com", "bobik");
+        userService.add(userBob);
+
+        User userAlice = new User("alice.alicenko@gmail.com", "alicka");
+        userService.add(userAlice);
+
+        userService.listUsers().forEach(System.out::println);
+    }
 }
---
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -0,0 +1,39 @@
+package mate.academy.spring.config;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@ComponentScan(basePackages = "mate.academy.spring")
+public class AppConfig {
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/ticket_app?serverTimezone=UTC");
+        basicDataSource.setUsername("root");
+        basicDataSource.setPassword("19cience19");
+        return basicDataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean localSessionFactoryBean =
+                new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(getDataSource());
+
+        Properties properties = new Properties();
+        properties.put("show_sql", "true");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        localSessionFactoryBean.setHibernateProperties(properties);
+
+        localSessionFactoryBean.setPackagesToScan("mate.academy.spring.model");
+
+        return localSessionFactoryBean;
+    }
+}
