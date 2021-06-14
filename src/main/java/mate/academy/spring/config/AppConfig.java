2fc0ad1fc0c36a82e54a95c72d4fed013038bec2
Ksenia <ksenia.obertii@gmail.com>
2021-06-14 16:15:14 +0300

    added application.properties and fixed User.toString

--- src/main/java/mate/academy/spring/config/AppConfig.java
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -3,21 +3,28 @@ package mate.academy.spring.config;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "mate.academy.spring")
 public class AppConfig {
+    @Autowired
+    private Environment environment;
+
     @Bean
     public DataSource getDataSource() {
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        basicDataSource.setUrl("jdbc:mysql://localhost:3306/ticket_app?serverTimezone=UTC");
-        basicDataSource.setUsername("root");
-        basicDataSource.setPassword("19cience19");
+        basicDataSource.setDriverClassName(environment.getProperty("db.driver"));
+        basicDataSource.setUrl(environment.getProperty("db.url"));
+        basicDataSource.setUsername(environment.getProperty("db.user"));
+        basicDataSource.setPassword(environment.getProperty("db.password"));
         return basicDataSource;
     }
 
@@ -28,8 +35,8 @@ public class AppConfig {
         localSessionFactoryBean.setDataSource(getDataSource());
 
         Properties properties = new Properties();
-        properties.put("show_sql", "true");
-        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
         localSessionFactoryBean.setHibernateProperties(properties);
 
         localSessionFactoryBean.setPackagesToScan("mate.academy.spring.model");
