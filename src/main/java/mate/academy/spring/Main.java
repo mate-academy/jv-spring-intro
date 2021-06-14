32da96d29ed1a83af107647f76d4169a76864be8
unknown <vovazalli@gmail.com>
2021-06-14 13:46:12 +0300

    first solution

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,25 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User john = new User();
+        john.setUserName("John");
+        
+        User mark = new User();
+        mark.setUserName("Mark");
+        
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        
+        userService.add(john);
+        userService.add(mark);
+        
+        userService.listUsers().forEach(System.out::println);
+    }
 }
