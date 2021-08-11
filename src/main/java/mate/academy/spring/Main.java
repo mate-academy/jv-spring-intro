3315737a4e6bad4224fe1c2f199b6252131b3afa
Borys Skvortsov <Dhakmar@gmail.com>
2021-08-11 20:23:40 +0300

    Filled Main

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,19 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        UserService userService = context.getBean(UserService.class);
+        User richard = new User();
+        richard.setName("Richard");
+        userService.add(richard);
+        System.out.println(userService.getAll());
+    }
 }
