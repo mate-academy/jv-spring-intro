bbc694380335cc10688f860c3d193484fd8c8c6b
Denis <deniskichan@gmail.com>
2021-08-13 22:07:20 +0300

    implemented userDao and userService and used Hibernate to test them

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,23 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.setEmail("asdasdas");
+        user1.setPassword("qwerty");
+        User user2 = new User();
+        user2.setEmail("user2");
+        user2.setPassword("qwerty");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(user1);
+        userService.add(user2);
+        System.out.println(userService.getAll());
+    }
 }
---
+++ src/main/java/mate/academy/spring/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package mate.academy.spring.service;
+
+import java.util.List;
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.model.User;
+import mate.academy.spring.util.HashUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public User add(User user) {
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(user.getPassword(), user.getSalt()));
+        return userDao.add(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+}
