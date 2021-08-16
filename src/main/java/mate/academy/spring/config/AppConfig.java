63c58270654df7f6f1f300cea527342124335241
nikdagaev <nikdagaev@gmail.com>
2021-08-16 16:08:21 +0300

    application.properties file added.

--- src/main/java/mate/academy/spring/config/AppConfig.java
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -6,18 +6,27 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "mate.academy.spring")
 public class AppConfig {
+    private Environment environment;
+
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/ticket_app?serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("nikita199");
+        dataSource.setDriverClassName(environment.getProperty("db.driver"));
+        dataSource.setUrl(environment.getProperty("db.url"));
+        dataSource.setUsername(environment.getProperty("db.username"));
+        dataSource.setPassword(environment.getProperty("db.password"));
         return dataSource;
     }
 
@@ -35,5 +44,4 @@ public class AppConfig {
 
         return localSessionFactoryBean;
     }
-
 }
