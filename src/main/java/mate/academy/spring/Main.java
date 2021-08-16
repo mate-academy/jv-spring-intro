49108c768056503ad20c231aaafe07990b46d9b7
vKutik <84785564+vKutik@users.noreply.github.com>
2021-08-16 15:51:39 +0300

    add logic

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,18 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(new User("vova","123123"));
+        userService.add(new User("shefir","123123"));
+        userService.add(new User("stas","123123"));
+        System.out.println(userService.getAll());
+    }
 }
