e82a7ee4c771ba437e2b603630239c2e546d8edf
EugeneSinica <eugenesinica@gmail.com>
2021-08-16 14:20:09 +0300

    implemented solution

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,23 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context
+            = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        User user1 = new User("Eugene", "Sinitsa");
+        User user2 = new User("Sinitsa", "Eugene");
+
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(user1);
+        userService.add(user2);
+
+        userService.getAll().forEach(System.out::println);
+    }
 }
