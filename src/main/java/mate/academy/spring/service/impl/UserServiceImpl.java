a1d29b9f4b161cdc215c259ee0fb6cfb60070a52
Yura Lug <LUV97@ukr.net>
2021-08-16 14:15:08 +0300

    First reliz

---
+++ src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package mate.academy.spring.service.impl;
+
+import java.util.List;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceImpl implements UserService {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserServiceImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save user: " + user + " to DB!", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM User", User.class).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get users from DB!", e);
+        }
+    }
+}
