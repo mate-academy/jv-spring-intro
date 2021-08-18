5a297b5c201d2f62b23e6adce353c1a298c6424f
olekskov <oleksandr@kovalenko.cc>
2021-08-18 20:59:36 +0000

    JV-Spring-Intro-solution
    Initial solution:
    - Implemented User model/dao/service level
    - Configured dependency injection and dataSource with Spring
    - DB/Hibernate settings taken from .properties file
    - Tested in PSVM

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,20 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        UserService userService = context.getBean(UserService.class);
+        User userBob = new User("Bob", "1111");
+        User userAlice = new User("Alice", "2222");
+        userService.add(userBob);
+        userService.add(userAlice);
+        userService.getAll().forEach(System.out::println);
+    }
 }
