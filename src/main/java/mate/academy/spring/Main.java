b3cbb4ab724c0a9813c01563cb05bc4f2891b2cf
Viktor Yukhno <pingvinomania@gmail.com>
2021-09-27 03:39:11 +0300

    implemented UserService and UserDao, configured ApplicationContext and DataSource

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,23 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.setName("Viktor");
+        user1.setSurname("Yukhno");
+        User user2 = new User();
+        user2.setName("Bob");
+        user2.setSurname("Ivanov");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(user1);
+        userService.add(user2);
+        System.out.println(userService.getAll());
+    }
 }
