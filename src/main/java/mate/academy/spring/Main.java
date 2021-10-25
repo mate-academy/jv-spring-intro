65050f78af077294cdd65dce683a2bce27bcdef2
Yuliia Mysova-Shreder <yuliiamysova@gmail.com>
2021-10-25 15:40:33 +0300

    Spring config created

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,28 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        User jaina = new User();
+        jaina.setName("Jaina");
+        jaina.setUsername("Proudmoore");
+        jaina.setEmail("email@newone.com");
+
+        User arthas = new User();
+        arthas.setName("Arthas");
+        arthas.setUsername("Menetil");
+        arthas.setEmail("lichKing@prince.com");
+
+        UserService userService = context.getBean(UserService.class);
+        userService.add(jaina);
+        userService.add(arthas);
+        userService.getAll().forEach(System.out::println);
+    }
 }
