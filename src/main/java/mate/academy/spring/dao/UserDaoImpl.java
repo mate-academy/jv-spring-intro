34a5d87962994b09c2f722d8275b4bb9fdf57114
Yehor Zapara <egor.zapara1994@gmail.com>
2021-11-02 17:05:58 +0200

    Checkstyle mistakes were fixed

--- src/main/java/mate/academy/spring/dao/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/UserDaoImpl.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserDaoImpl implements  UserDao {
+public class UserDaoImpl implements UserDao {
     private final SessionFactory sessionFactory;
 
     @Autowired
--- src/main/java/mate/academy/spring/service/UserServiceImpl.java
+++ src/main/java/mate/academy/spring/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements  UserService {
+public class UserServiceImpl implements UserService {
     private UserDao userDao;
 
     @Autowired
