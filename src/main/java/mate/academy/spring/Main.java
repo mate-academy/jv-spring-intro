66aa9b9f1037d968f5572131fc9e9d0be3487fd6
Denys Stefanko <denys.stefanko@gmail.com>
2021-11-02 18:10:44 +0200

    add task

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,17 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService bean = context.getBean(UserService.class);
+        bean.add(new User("Petro", "Petrenko"));
+        bean.getAll().forEach(System.out::println);
+
+    }
 }
