954bbb32ec82e608fca31d8660c74f71c3ee5107
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-11-03 21:14:41 +0200

    correct for travis1

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -1,7 +1,5 @@
 package mate.academy.spring.model;
 
-import org.springframework.stereotype.Component;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
