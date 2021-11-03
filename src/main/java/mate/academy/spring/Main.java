d412ac9b189e83fea6c40105096569682161cc3d
SolnalarmJulia <solnalarm@meta.ua>
2021-11-03 23:26:34 +0200

    refactoring

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -6,9 +6,10 @@ import mate.academy.spring.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+private static final AnnotationConfigApplicationContext context =
+        new AnnotationConfigApplicationContext(AppConfig.class);
+
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
         UserService userService = context.getBean(UserService.class);
         User user = new User();
         user.setEmail("test@ukr.net");
