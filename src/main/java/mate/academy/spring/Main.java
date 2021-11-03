2e3501f5402febdcb78cf8db84e0c92d360c67b5
Alexandr <netiazhevskyi@gmail.com>
2021-11-03 19:01:59 +0200

    dao and service implemented. added app.properties and appconfig

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,20 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("UserName");
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(user);
+        System.out.println(userService.getAll());
+
+    }
 }
