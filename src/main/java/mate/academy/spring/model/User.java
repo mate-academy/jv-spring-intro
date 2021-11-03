36dbda2ce4dac1f5cea677b14702a625b0af3ee4
Kyrylo Dvornyk <kiril75341@gmail.com>
2021-11-03 15:47:40 +0200

    Implemented java-spring-intro task

---
+++ src/main/java/mate/academy/spring/model/User.java
@@ -0,0 +1,40 @@
+package mate.academy.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("username='").append(username).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
