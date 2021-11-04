81dea4e266a5ff5ee094f1412105b2607f0d300b
P.Zbarazkyy <mtu09@ukr.net>
2021-11-04 13:11:06 +0200

    Created User model and levels service, dao. Used technology Spring

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,24 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context
+            = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        User userA = new User();
+        userA.setEmail("A@i.ua");
+        userA.setPassword("aaaaa");
+        User userB = new User();
+        userB.setEmail("B@i.ua");
+        userB.setPassword("bbbb");
+        UserService bean = context.getBean(UserService.class);
+        bean.add(userA);
+        bean.add(userB);
+        bean.getAll().forEach(System.out::println);
+    }
 }
