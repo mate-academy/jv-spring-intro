e4c9ace031fb6fca0a7970cb90a75efe69e04966
SolnalarmJulia <solnalarm@meta.ua>
2021-11-04 10:42:54 +0200

    refactoring

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -6,8 +6,8 @@ import mate.academy.spring.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-private static final AnnotationConfigApplicationContext context =
-        new AnnotationConfigApplicationContext(AppConfig.class);
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
 
     public static void main(String[] args) {
         UserService userService = context.getBean(UserService.class);
