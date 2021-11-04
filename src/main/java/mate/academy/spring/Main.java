d41af66bca8a98d0665f136f0e543e59f676db15
Taras Oleksiuk <64101459+taras-oleksiuk@users.noreply.github.com>
2021-11-04 15:36:57 +0200

    The first commit.

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,23 @@
 package mate.academy.spring;
 
+import java.util.List;
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User userBob = new User();
+        userBob.setAge(22);
+        userBob.setName("Bob");
+
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(userBob);
+        List<User> allUsers = userService.getAll();
+        System.out.println(allUsers);
+    }
 }
