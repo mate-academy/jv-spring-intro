fff50fefeded90671a7f8e24eff962ecd4138f12
Denys Smei <denys.smei@artcodevision.com>
2021-11-04 16:32:18 +0200

    testing spring with simple UserService

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,22 @@
 package mate.academy.spring;
 
+import java.util.List;
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        User admin = new User("root@gmail.com", "root");
+
+        UserService userService = context.getBean(UserService.class);
+        userService.add(admin);
+
+        List<User> userList = userService.getAll();
+        userList.stream().forEach(System.out::println);
+    }
 }
