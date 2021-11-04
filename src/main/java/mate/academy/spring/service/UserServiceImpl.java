11519c32607c53c20c27e96def0d5ac442f6c179
V <v.ivashchenko42@protonmail.com>
2021-11-04 14:31:11 +0300

    added spring User

---
+++ src/main/java/mate/academy/spring/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package mate.academy.spring.service;
+
+import java.util.List;
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+}
