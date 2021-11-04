bca08928f240b3de5a6d62b94c41537bc79b5df1
Serhii Blakh <s.a.blakh@ukr.net>
2021-11-04 09:52:20 +0200

    created User, UserDao and UserService saved user to DB

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,19 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User userOne = new User();
+        userOne.setName("Bob");
+        userOne.setEmail("guru@mail.com");
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(userOne);
+        System.out.println(userService.getAll());
+    }
 }
