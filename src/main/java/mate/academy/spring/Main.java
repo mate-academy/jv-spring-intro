9a22201fdc863101576153a77ca5e84a30c50116
Chupiak <chupyak@gmail.com>
2021-11-23 19:10:49 +0200

    1.0V Solution "spring intro"

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,32 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService beanUserService = context.getBean(UserService.class);
+
+        User user1 = new User();
+        user1.setName("Bob");
+        user1.setAge(22L);
+        beanUserService.add(user1);
+
+        User user2 = new User();
+        user2.setName("Alice");
+        user2.setAge(16L);
+        beanUserService.add(user2);
+
+        User user3 = new User();
+        user3.setName("John");
+        user3.setAge(43L);
+        beanUserService.add(user3);
+
+        System.out.println(beanUserService.getAll());
+
+    }
 }
