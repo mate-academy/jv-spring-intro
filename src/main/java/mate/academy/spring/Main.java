659eae17a5e31e26247ef4b9ca58e317fba65511
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-12-21 13:27:40 +0200

    Java Spring Intro solution added

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,24 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User john = new User();
+        john.setName("John");
+        john.setAge(32);
+
+        System.out.println(john.toString());
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(john);
+
+        System.out.println(userService.getAll());
+    }
 }
