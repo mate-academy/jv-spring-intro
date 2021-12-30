8ea40263f0c1eb0892be77fe897fdfd468a884aa
Zachar Chorny <ufumoveldar@gmail.com>
2021-12-30 21:25:16 +0200

    Service and Dao working layers created using Spring

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,23 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.setName("Bob");
+        User user2 = new User();
+        user2.setName("Alice");
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(user1);
+        userService.add(user2);
+        userService.getAll().forEach(System.out::println);
+    }
 }
---
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -0,0 +1,42 @@
+package mate.academy.spring.config;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "mate.academy.spring")
+public class AppConfig {
+    @Autowired
+    private Environment enviroment;
+
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(enviroment.getProperty("db.driver"));
+        dataSource.setUrl(enviroment.getProperty("db.url"));
+        dataSource.setUsername(enviroment.getProperty("db.username"));
+        dataSource.setPassword(enviroment.getProperty("db.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean localSession = new LocalSessionFactoryBean();
+        localSession.setDataSource(getDataSource());
+        Properties properties = new Properties();
+        properties.put("show_sql", enviroment.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", enviroment.getProperty("hibernate.hbm2ddl.auto"));
+        localSession.setHibernateProperties(properties);
+        localSession.setPackagesToScan("mate.academy.spring.model");
+        return localSession;
+    }
+}
