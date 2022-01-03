d2198b1dcec9658d6b2cc7e006216818bd673f45
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2022-01-04 00:26:55 +0200

    was implemented UserDao and UserService layers

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,16 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = applicationContext.getBean(UserService.class);
+        userService.add(new User("username", "awesome password"));
+        System.out.println(userService.getAll());
+    }
 }
