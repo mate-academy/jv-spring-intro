7e66a5fd8d16e7dc1a97894b63aac2361c2d3caf
Andriy-dz <andriydzdz@gmail.com>
2022-01-03 16:12:56 +0200

    implemented the project on the spring

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,20 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User user = new User("Qwe","Rty");
+        User user1 = new User("Asd","Fgh");
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(user);
+        userService.add(user1);
+        System.out.println(userService.getAll());
+    }
 }
