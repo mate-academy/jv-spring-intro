4a9dce4c4cf6a3015999758e0f968773a444f75c
Anton Volskyi <antonvolskyi1234@gmail.com>
2022-01-03 18:12:12 +0200

    User model is implemented using a spring

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,18 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(new User("John", "qwerty"));
+        userService.add(new User("Alice", "12345"));
+        System.out.println(userService.getAll());
+
+    }
 }
