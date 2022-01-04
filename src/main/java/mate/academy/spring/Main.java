6fc0039a730f43eed58f55673b534926f409dc9a
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2022-01-04 14:51:29 +0200

    corrections after student review

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -6,11 +6,12 @@ import mate.academy.spring.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+    private static final AnnotationConfigApplicationContext applicationContext =
+            new AnnotationConfigApplicationContext(AppConfig.class);
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
         UserService userService = applicationContext.getBean(UserService.class);
         userService.add(new User("username", "awesome password"));
+        userService.add(new User("Carl", "with awesome password"));
         System.out.println(userService.getAll());
     }
 }
--- src/main/java/mate/academy/spring/dao/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/UserDaoImpl.java
@@ -41,8 +41,8 @@ public class UserDaoImpl implements UserDao {
     @Override
     public List<User> getAll() {
         try (Session session = sessionFactory.openSession()) {
-            Query<User> getAllUsers = session.createQuery("from User", User.class);
-            return getAllUsers.getResultList();
+            Query<User> query = session.createQuery("from User", User.class);
+            return query.getResultList();
         } catch (HibernateException e) {
             throw new RuntimeException("Can't get all users from DB", e);
         }
