6b7161fd6961924294ac017c1d393fe0aa1a2e7e
Sergii Svystun <sergiisvystun@gmail.com>
2022-01-04 20:02:39 +0200

    initial commit

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,24 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        User userFirst = new User();
+        userFirst.setAge(20);
+        userFirst.setName("Bob");
+        User userSecond = new User();
+        userSecond.setAge(40);
+        userSecond.setName("Alice");
+        UserService userService = context.getBean(UserService.class);
+        userService.add(userFirst);
+        userService.add(userSecond);
+        System.out.println(userService.getAll());
+    }
 }
