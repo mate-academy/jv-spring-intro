ee6e8a973d6249f6712ac826cf6acce81e411d77
Anatolii Huryk <tolik.mario96@gmail.com>
2022-01-05 16:04:57 +0200

    create constant main class

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -3,9 +3,13 @@ package mate.academy.spring;
 import mate.academy.spring.config.AppConfig;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+    private static final ApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
     public static void main(String[] args) {
         User bob = new User();
         bob.setName("Bob");
@@ -14,9 +18,6 @@ public class Main {
         User alice = new User();
         alice.setName("Alice");
         alice.setEmail("alice@gmail.com");
-
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
 
         UserService userService = context.getBean(UserService.class);
         userService.add(bob);
