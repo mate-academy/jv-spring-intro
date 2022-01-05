9e431834929c91cf6ded098f5f1c43cb59397c39
Panchenko Volodymyr <volodymyr.panchenko147@egmail.com>
2022-01-05 16:45:19 +0200

    hibernate.cfg.xml was change to application.properties

--- src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
@@ -1,26 +1,32 @@
 package mate.academy.spring.dao.impl;
 
 import java.util.List;
-import javax.persistence.criteria.CriteriaQuery;
 import mate.academy.spring.dao.UserDao;
 import mate.academy.spring.model.User;
-import mate.academy.spring.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDaoImpl implements UserDao {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
-    public User add(User user) {
+    public void add(User user) {
         Session session = null;
         Transaction transaction = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = sessionFactory.openSession();
             transaction = session.beginTransaction();
             session.save(user);
             transaction.commit();
-            return user;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
@@ -36,11 +42,8 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public List<User> getAll() {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            CriteriaQuery<User> query = session.getCriteriaBuilder()
-                    .createQuery(User.class);
-            query.from(User.class);
-            return session.createQuery(query).getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM User",User.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Can't get all users from DB ", e);
         }
