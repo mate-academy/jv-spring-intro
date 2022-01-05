e49fd13f960e780c05581df386dfb6e89db0cce8
Konstantin <www.kosta.kon@gmail.com>
2022-01-05 15:43:08 +0200

    Context type was changed to interface.

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -3,10 +3,11 @@ package mate.academy.spring;
 import mate.academy.spring.config.AppConfig;
 import mate.academy.spring.model.User;
 import mate.academy.spring.service.UserService;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-    private static final AnnotationConfigApplicationContext context
+    private static final ApplicationContext context
             = new AnnotationConfigApplicationContext(AppConfig.class);
 
     public static void main(String[] args) {
