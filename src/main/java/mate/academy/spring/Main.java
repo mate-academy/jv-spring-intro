c2785d154f95464d7d555ff1151e10ca7f29c612
Oleg Shiriaev <mystidreamer@gmail.com>
2022-01-05 23:41:11 +0200

    I was hinted by idea to use constructors

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -6,14 +6,15 @@ import mate.academy.spring.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+    private static final AnnotationConfigApplicationContext context
+            = new AnnotationConfigApplicationContext(AppConfig.class);
+
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context
-                = new AnnotationConfigApplicationContext(AppConfig.class);
-        UserService userService = context.getBean(UserService.class);
         User valera = new User();
         valera.setName("Valera");
         User egorka = new User();
         egorka.setName("Egorka");
+        UserService userService = context.getBean(UserService.class);
         userService.add(valera);
         userService.add(egorka);
         System.out.println(userService.getAll());
