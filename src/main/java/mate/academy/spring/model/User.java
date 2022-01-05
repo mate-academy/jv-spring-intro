4e751dea5bd55263275db0b00b3f4bd94b2bbc0f
Pavlo_Martsiniv <imitator2010@gmail.com>
2022-01-05 21:32:48 +0200

    implemented task spring-intro

---
+++ src/main/java/mate/academy/spring/model/User.java
@@ -0,0 +1,25 @@
+package mate.academy.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+}
