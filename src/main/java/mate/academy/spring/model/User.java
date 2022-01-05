997bc7fceff8ab7c241be5a72998eb09103ce4c9
Olha Tankevych <freakystyley85@gmail.com>
2022-01-05 17:19:17 +0200

    changed method toString

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -51,10 +51,10 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
+        return "User{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", lastName='" + lastName + '\''
+                + '}';
     }
 }
