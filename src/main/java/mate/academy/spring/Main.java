4f49e65ce219addd04ef074bfe8d9cafb3db4fba
Oleksandr Ostapchuk <shurion.ua@gmail.com>
2022-01-05 22:57:58 +0200

    Project created

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,21 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        User bob = new User();
+        bob.setName("Bob");
+        User alice = new User();
+        alice.setName("Alice");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(bob);
+        userService.add(alice);
+        System.out.println(userService.getAll());
+    }
 }
