b3ff0e5ec2511e58948885e37f8e17f421fce196
VitaliiMarchenko <vitalbka.01@gmail.com>
2022-01-05 16:19:27 +0200

    initial commit

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,27 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        final UserService userService = context.getBean(UserService.class);
+
+        User bob = new User();
+        bob.setName("Bob");
+        bob.setAge(14);
+
+        User alice = new User();
+        alice.setName("Alice");
+        alice.setAge(15);
+
+        userService.add(bob);
+        userService.add(alice);
+        userService.getAll().forEach(System.out::println);
+    }
 }
