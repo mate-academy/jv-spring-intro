5be70590d12b0c42c038ef13bc3fc2191e01ae15
Dany Nesterov <nesterov1200997@gmail.com>
2022-01-06 00:22:29 +0200

    constant were added

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -7,11 +7,11 @@ import mate.academy.spring.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+    private static final AnnotationConfigApplicationContext CONTEXT =
+            new AnnotationConfigApplicationContext(AppConfig.class);
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        UserService userService = context.getBean(UserService.class);
+        UserService userService = CONTEXT.getBean(UserService.class);
 
         User danya = new User();
         danya.setAge(24);
