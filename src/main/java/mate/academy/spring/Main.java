1f86e627ce769c2943ed5a8fb874c2f9108a878b
SarumanIsAlive <90073141+SarumanIsAlive@users.noreply.github.com>
2022-01-06 09:44:57 +0200

    Implement UserService and UserDao Configured DataSource Saving Users into DB and reading they back

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,22 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        User alice = new User("Alice", 22, true);
+
+        User bob = new User("Bob", 45, false);
+
+        UserService userService = context.getBean(UserService.class);
+        userService.add(alice);
+        userService.add(bob);
+        System.out.println(userService.getAll());
+    }
 }
