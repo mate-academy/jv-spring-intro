8bdaddcba000eeef51da5267a2c92602dde761eb
Kate Deshchynska <katerinka9636@gmail.com>
2022-01-06 14:19:30 +0200

    create user model, dao and service layer with spring

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,21 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        UserService userService = context.getBean(UserService.class);
+
+        User user = new User();
+        user.setEmail("kate@gmail.com");
+        user.setPassword("11111111");
+        userService.add(user);
+        userService.getAll().forEach(System.out::println);
+    }
 }
