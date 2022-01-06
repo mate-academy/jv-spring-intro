1ce471098b2e88bd6c15f2111e58e357adc078a9
Eugene-exe <zhekacauselove@gmail.com>
2022-01-06 13:56:44 +0200

    Fix empty line

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -7,8 +7,6 @@ import mate.academy.spring.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-
-
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
