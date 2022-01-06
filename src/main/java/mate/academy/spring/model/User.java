bea6ae040f49a017e88b0350a14930ee50a9e5d2
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2022-01-06 15:48:54 +0200

    spring intro

---
+++ src/main/java/mate/academy/spring/model/User.java
@@ -0,0 +1,26 @@
+package mate.academy.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String name;
+    @NonNull
+    private String lastName;
+}
