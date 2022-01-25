4099e89d245067a92fbc2118e8a539c4be4cc57a
ASelivonchyk <aselivonchyk@gmail.com>
2022-01-25 21:21:55 +0200

    Wrote an assignment jv-spring-intro. - added spring, hibernate & mysql-driver dependencies to pom.xml; - added application.properties file; - created AppConfig.class; - added POJO User; - added UserDao and its impl with methods add(), getAll(); - added UserService and its impl with methods add(), getAll(); - in Main class tested workability.

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,18 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        UserService userService = context.getBean(UserService.class);
+        userService.add(new User("Bob", "first@ukr.net", "qwerty"));
+        userService.add(new User("Alice", "second@ukr.net", "12345"));
+        userService.getAll().forEach(System.out::println);
+    }
 }
---
+++ src/main/java/mate/academy/spring/dao/UserDao.java
@@ -0,0 +1,12 @@
+package mate.academy.spring.dao;
+
+import java.util.List;
+import mate.academy.spring.model.User;
+
+public interface UserDao {
+
+    User add(User user);
+
+    List<User> getAll();
+
+}
