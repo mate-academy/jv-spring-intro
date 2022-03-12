2d8ae5f813034619d7abf12f7733a57c5a751eeb
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-03-12 21:21:50 +0200

    Add Main.main

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,4 +1,19 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        User user = new User();
+        user.setEmail("sample@gmail.com");
+        user.setPassword("123456");
+        userService.add(user);
+        System.out.println(userService.getAll());
+
+    }
 }
