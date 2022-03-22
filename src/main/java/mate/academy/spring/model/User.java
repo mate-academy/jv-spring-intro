431f2d51d24f7e5f81e396f4fc7e67b20395b267
sviat-h <svyatoslav.grinik@gmail.com>
2022-03-22 03:29:38 +0200

    added User entity

---
+++ src/main/java/mate/academy/spring/model/User.java
@@ -0,0 +1,30 @@
+package mate.academy.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String name;
+    @NonNull
+    private int age;
+}
