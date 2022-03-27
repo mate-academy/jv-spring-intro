e8bf213222dff28577f11246ee239e228288dbb5
Viacheslav Cheban <vcheb@eg.dk>
2022-03-27 10:58:00 +0300

    add user, UserDao, UserService

---
+++ src/main/java/mate/academy/spring/service/UserService.java
@@ -0,0 +1,11 @@
+package mate.academy.spring.service;
+
+import mate.academy.spring.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+
+    List<User> getAll();
+}
---
+++ src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package mate.academy.spring.service.impl;
+
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+}
