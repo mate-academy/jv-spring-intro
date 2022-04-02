84006d4b1ab62a7e42c40d97004fe620a67b1f30
Vadym <vadya829@gmail.com>
2022-04-02 17:24:50 +0200

    checkstyle fix

--- src/main/java/mate/academy/spring/service/UserService.java
+++ src/main/java/mate/academy/spring/service/UserService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import mate.academy.spring.model.User;
 
 public interface UserService {
-      User add(User user);
+    User add(User user);
   
-      List<User> getAll();
+    List<User> getAll();
 }
