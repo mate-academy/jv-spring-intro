8d41847be4f532e5ad031a8af9fe497079c93527
Roman <dudanecr@gmail.com>
2022-04-18 18:56:59 +0300

    created AppConfig class, add user model, dao and service layer

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,25 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
 
+        User userOne = new User();
+        userOne.setName("userOne");
+        userOne.setAge(20);
+        userService.add(userOne);
+
+        User userTwo = new User();
+        userTwo.setName("userTwo");
+        userTwo.setAge(21);
+        userService.add(userTwo);
+
+        System.out.println(userService.getAll());
     }
 }
