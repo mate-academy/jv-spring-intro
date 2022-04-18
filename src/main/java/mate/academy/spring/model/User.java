5fd564fc54a9fb72189a8c4d13cfae06aedd97c6
Oleh Mytiai <mytiai.ov@icloud.com>
2022-04-18 14:01:19 +0300

    implemented UserService, added constructor to User model, tested solution in main method

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -16,6 +16,14 @@ public class User {
     private String name;
     private int age;
 
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public Long getId() {
         return id;
     }
