5ab6099022fc933a0fd05e4013c7ac27760d4dcb
sltnsrh <lifenjoy8888@gmail.com>
2022-04-18 18:34:54 +0300

    spring intro implementation

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,23 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        User userBob = new User();
+        userBob.setName("Bob");
+        userBob.setAge(18);
+        userService.add(userBob);
+        User userAlice = new User();
+        userBob.setName("Alice");
+        userBob.setAge(40);
+        userService.add(userAlice);
+        userService.getAll().forEach(System.out::println);
     }
 }
