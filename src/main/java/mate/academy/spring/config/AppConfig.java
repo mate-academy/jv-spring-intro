76ff7c93d8ba5e6ced75f858bf9ce7c9fe6e4174
Maksym Pokras <mxpokras@gmail.com>
2022-04-24 13:45:28 +0200

    sessionFactory bean created

--- src/main/java/mate/academy/spring/config/AppConfig.java
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -2,6 +2,7 @@ package mate.academy.spring.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
@@ -9,11 +10,12 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@ComponentScan(basePackages = "mate.academy.spring")
 public class AppConfig {
 //    @Bean
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.myysql.cj.jdbc.Driver");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/ticket_app");
         dataSource.setUsername("root");
         dataSource.setPassword("root");
@@ -27,6 +29,7 @@ public class AppConfig {
         Properties properties = new Properties();
         properties.put("show_sql", "true");
         properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
         localSessionFactoryBean.setHibernateProperties(properties);
         localSessionFactoryBean.setPackagesToScan("mate.academy.spring.model");
         return localSessionFactoryBean;
