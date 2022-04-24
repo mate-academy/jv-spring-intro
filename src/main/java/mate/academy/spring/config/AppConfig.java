09233bd9cc5414e5413261c4f24b5d5d9229076b
Maksym Pokras <mxpokras@gmail.com>
2022-04-24 12:45:44 +0200

    AppConfig created

---
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -0,0 +1,34 @@
+package mate.academy.spring.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class AppConfig {
+//    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.myysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/ticket_app");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(getDataSource());
+        Properties properties = new Properties();
+        properties.put("show_sql", "true");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        localSessionFactoryBean.setHibernateProperties(properties);
+        localSessionFactoryBean.setPackagesToScan("mate.academy.spring.model");
+        return localSessionFactoryBean;
+    }
+}
