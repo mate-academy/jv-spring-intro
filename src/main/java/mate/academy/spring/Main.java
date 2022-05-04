dfb3c66b333bae7354f1a16cb9de9b02a28051b4
Viktor Stadnik <stadhik.victor@gmail.com>
2022-05-04 22:34:51 +0300

    The file info was completed

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,23 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
 
+        User bob = new User("Bob",25);
+        User alice = new User("Alice",23);
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(bob);
+        userService.add(alice);
+
+        System.out.println(userService.getAll());
     }
 }
