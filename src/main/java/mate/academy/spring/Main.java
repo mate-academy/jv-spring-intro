16a43994f426b89e4868007bc94760fae680cfee
oleg-new <9213195@gmail.com>
2022-05-28 22:08:09 +0300

    class User created created interfaces UserService, UserDao and classes implementing them annotations @Bean, @Autowired, @Service, @Repository   placed for correct operation

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,24 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+
+    public static void main(String[] args) {
+        User goga = new User();
+        goga.setName("Goga");
+        goga.setAge(19);
+        User magoga = new User();
+        magoga.setName("Magoga");
+        magoga.setAge(20);
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        System.out.println(userService.add(goga));
+        System.out.println(userService.add(magoga));
+        System.out.println(userService.getAll());
     }
 }
