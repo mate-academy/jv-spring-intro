aaab1239fe0e8ee5f69344e269a64af471262d29
Serhii Pankov <pankoff.serg@gmail.com>
2022-06-03 16:02:49 +0300

    add solution for jv-spring-intro

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,27 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
+        UserService userService = context.getBean(UserService.class);
+
+        User userSerhii = new User();
+        userSerhii.setAge(40);
+        userSerhii.setName("Serhii");
+        userService.add(userSerhii);
+
+        User userDamir = new User();
+        userDamir.setAge(1);
+        userDamir.setName("Damir");
+        userService.add(userDamir);
+
+        userService.getAll().forEach(System.out::println);
     }
 }
