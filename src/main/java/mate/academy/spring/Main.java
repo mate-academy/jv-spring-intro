9331013d6595b3344098807ddd4ccc85239613e6
Nickolai Vakulich <nickolai.vakulich@gmail.com>
2022-06-09 16:19:53 +0300

    Added -User class -UserDao and implementation -UserService and implementation -configure hibernate and spring

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,23 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.model.User;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        UserDao userDao = context.getBean(UserDao.class);
+
+        User user = new User();
+        user.setAge(30);
+        user.setName("Username");
+
+        System.out.println(userDao.add(user));
+
+        userDao.getAll().forEach(System.out::println);
     }
 }
