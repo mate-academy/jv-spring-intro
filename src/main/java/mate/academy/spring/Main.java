986e5e5352cdfdba6f813b3b3516b45105ced1c2
OLEH VIESHAIEV <sred_a@i.ua>
2022-06-09 13:46:43 +0300

    jv-spring-intro solution was created

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,27 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        User alice = new User();
+        alice.setName("Alice");
+        alice.setAge(30);
+        userService.add(alice);
+
+        User john = new User();
+        john.setName("John");
+        john.setAge(45);
+        userService.add(john);
+
+        userService.getAll().forEach(System.out::println);
     }
 }
