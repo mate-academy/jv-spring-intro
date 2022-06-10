79d0851b7cc8ce40d0b6db904d9b4c11ba860fea
mordast7 <mordast7@gmail.com>
2022-06-10 18:55:32 +0300

    solved task

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,25 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+
+    public static void main(String[] args) {
+        User alice = new User();
+        alice.setAge(27);
+        alice.setName("Alice");
+        User bob = new User();
+        bob.setName("Bob");
+        bob.setAge(29);
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(alice);
+        userService.add(bob);
+        userService.getAll().forEach(System.out::println);
     }
 }
