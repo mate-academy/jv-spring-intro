acfbb5466b987569c55db824bcd6bf8bf6667dc2
Denys.Sh <7860denis@gmail.com>
2022-06-30 15:32:50 +0300

    added User, AppConfig, UserDao, UserService

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,24 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        User den = new User();
+        den.setName("Den");
+        den.setAge(33);
 
+        User mak = new User();
+        mak.setName("Mak");
+        mak.setAge(11);
+
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        System.out.println("userService.getAll() = " + userService.getAll());
     }
 }
