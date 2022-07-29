13704f33b1f6d40502b960803bc86a39e1d37950
Mykola Yakymiv <mykola8426@gmail.com>
2022-07-29 21:21:07 +0300

    spring-homework

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,36 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        User anton = new User();
+        anton.setName("Anton");
+        anton.setAge(11);
 
+        User chris = new User();
+        chris.setName("Chris");
+        chris.setAge(11);
+
+        User den = new User();
+        den.setName("Den");
+        den.setAge(13);
+
+        User kate = new User();
+        kate.setName("Kate");
+        kate.setAge(22);
+
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(anton);
+        userService.add(chris);
+        userService.add(den);
+        userService.add(kate);
+
+        userService.getAll().forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/academy/spring/dao/UserDao.java
@@ -0,0 +1,11 @@
+package mate.academy.spring.dao;
+
+import java.util.List;
+import mate.academy.spring.model.User;
+import org.springframework.stereotype.Repository;
+
+public interface UserDao {
+    User add(User user);
+
+    List<User> getAll();
+}
