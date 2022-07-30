846071f4a2d61c3273dda8ba6a870390b694f988
Dima Podakin <calombo.podakin@ukr.net>
2022-07-30 14:38:22 +0300

    done spring intro

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,21 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.model.User;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+    private static final AnnotationConfigApplicationContext context
+            = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        UserDao userDao = context.getBean(UserDao.class);
+
+        User bob = new User();
+        bob.setAge(20);
+        bob.setName("Bob");
+        System.out.println(userDao.add(bob));
+        userDao.getAll().forEach(System.out::println);
     }
 }
