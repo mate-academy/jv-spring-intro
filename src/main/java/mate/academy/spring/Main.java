96e504a90beff29c3b344c8aa19c08039749bc6d
agash <alexgash8@gmail.com>
2022-07-31 23:10:31 +0300

    works

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,27 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
+        UserService userService = context.getBean(UserService.class);
+
+        User alex = new User();
+        alex.setName("alex");
+        alex.setAge(39);
+        userService.add(alex);
+
+        User serge = new User();
+        serge.setName("serge");
+        serge.setAge(44);
+        userService.add(serge);
+
+        userService.getAll().forEach(System.out::println);
     }
 }
