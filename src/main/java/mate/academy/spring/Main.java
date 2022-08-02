5d620bef353b74659d3cd146ae3741cbaa3309f6
Karina Tanasiichuk <k.tanasiichuk18@gmail.com>
2022-08-02 22:54:25 +0300

    spring intro

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,26 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
 
+        User karina = new User();
+        karina.setName("Karina");
+        karina.setAge(24);
+        User alex = new User();
+        alex.setName("Alex");
+        alex.setAge(25);
+
+        AnnotationConfigApplicationContext context = new
+                AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+        userService.add(karina);
+        userService.add(alex);
+        System.out.println(userService.getAll());
     }
 }
