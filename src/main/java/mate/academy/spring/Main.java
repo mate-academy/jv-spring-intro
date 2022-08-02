c19bc9fb92d1c6b31ca57100cf5dc4137d161098
Tymur Tsvietkov <tymur.tsvietkov@gmail.com>
2022-08-02 11:48:18 +0300

    hw-spring-intro

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,26 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        User firstUser = new User();
+        firstUser.setName("First user");
+        firstUser.setAge(18);
 
+        User secondUser = new User();
+        secondUser.setName("Second user");
+        secondUser.setAge(18);
+
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+        userService.add(firstUser);
+        userService.add(secondUser);
+        System.out.println(userService.getAll());
     }
 }
