12457814f6cc7bfa27c1a11ccb06805f94e14de8
Vladyslav Kondiievskyi <80918559+Vladyslav-Kondiievskyi@users.noreply.github.com>
2022-09-15 17:58:43 +0300

    created intro

--- src/main/java/mate/academy/spring/config/AppConfig.java
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -30,17 +30,16 @@ public class AppConfig {
 
     @Bean
     public LocalSessionFactoryBean getSessionFactory() {
-        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        LocalSessionFactoryBean localSessionFactoryBean =
+                new LocalSessionFactoryBean();
         localSessionFactoryBean.setDataSource(getDataSource());
 
         Properties properties = new Properties();
-        properties.put("show_sql", environment.getProperty("spring.jpa.show-sql"));
+        properties.put("show_sql", environment.getProperty("hibernate.show_sql"));
         properties.put("hibernate.hbm2ddl.auto",
-                environment.getProperty("spring.jpa.hibernate.ddl-auto"));
-        properties.put("hibernate.dialect",
-                environment.getProperty("hibernate.dialect"));
-
+                environment.getProperty("hibernate.hbm2ddl.auto"));
         localSessionFactoryBean.setHibernateProperties(properties);
+
         localSessionFactoryBean.setPackagesToScan("mate.academy.spring.model");
         return localSessionFactoryBean;
     }
