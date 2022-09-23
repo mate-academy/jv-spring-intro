a8f794e41338be6ae433547f8d54f7b2167346ac
Roman Pylypchuk <roman.pylypchuk@yahoo.com>
2022-09-23 18:28:27 +0300

    tested methods in Main.java class

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,26 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
 
+        User ann = new User();
+        ann.setName("Ann");
+        ann.setAge(25);
+        userService.add(ann);
+
+        User daniel = new User();
+        daniel.setName("Daniel");
+        daniel.setAge(27);
+        userService.add(daniel);
+
+        System.out.println(userService.getAll());
     }
 }
