fc00a94233c79773f629064fd6d73a7172c7d9df
Oleksii Akishev <akishev.oleksii@gmail.com>
2022-09-28 13:51:31 +0300

    Fulfilled implementation

---
+++ src/main/java/mate/academy/spring/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package mate.academy.spring.service;
+
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public User add(User user) {
+        return userDao.add(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+}
