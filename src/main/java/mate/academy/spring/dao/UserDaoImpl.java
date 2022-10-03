c24ba8c79c76839e61675ac73718e692e920ca87
Serhii Davydov <serhii.davydov@gmail.com>
2022-10-03 18:28:28 +0300

    Second commit

--- src/main/java/mate/academy/spring/dao/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/UserDaoImpl.java
@@ -1,21 +1,20 @@
-package mate.academy.spring.dao.impl;
+package mate.academy.spring.dao;
 
 import java.util.List;
-import mate.academy.spring.dao.UserDao;
-import mate.academy.spring.exception.DataProcessingException;
 import mate.academy.spring.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDaoImpl implements UserDao {
-    private final SessionFactory factory;
+    private final SessionFactory sessionFactory;
 
-    public UserDaoImpl(SessionFactory factory) {
-        this.factory = factory;
+    @Autowired
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
     @Override
@@ -23,7 +22,7 @@ public class UserDaoImpl implements UserDao {
         Session session = null;
         Transaction transaction = null;
         try {
-            session = factory.openSession();
+            session = sessionFactory.openSession();
             transaction = session.beginTransaction();
             session.save(user);
             transaction.commit();
@@ -32,7 +31,7 @@ public class UserDaoImpl implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can`t add user to db" + user, e);
+            throw new RuntimeException("Can't insert a user: " + user, e);
         } finally {
             if (session != null) {
                 session.close();
@@ -42,11 +41,10 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public List<User> getAll() {
-        try (Session session = factory.openSession()) {
-            Query<User> getAllUsers = session.createQuery("from User", User.class);
-            return getAllUsers.getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from User", User.class).getResultList();
         } catch (Exception e) {
-            throw new DataProcessingException("Can`t get all users from db", e);
+            throw new RuntimeException("Can't get all users", e);
         }
     }
 }
--- src/main/java/mate/academy/spring/service/UserService.java
+++ src/main/java/mate/academy/spring/service/UserService.java
@@ -1,2 +1,11 @@
-package mate.academy.spring.service;public class UserService {
+
+package mate.academy.spring.service;
+
+import java.util.List;
+import mate.academy.spring.model.User;
+
+public interface UserService {
+    User add(User user);
+
+    List<User> getAll();
 }
