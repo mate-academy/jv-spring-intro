6ea3cf9761139ad2592a5976b776eb30737dbcf9
Serhii Davydov <serhii.davydov@gmail.com>
2022-10-03 17:57:57 +0300

    First commit

---
+++ src/main/java/mate/academy/spring/dao/UserDaoImpl.java
@@ -0,0 +1,52 @@
+package mate.academy.spring.dao.impl;
+
+import java.util.List;
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private final SessionFactory factory;
+
+    public UserDaoImpl(SessionFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can`t add user to db" + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        try (Session session = factory.openSession()) {
+            Query<User> getAllUsers = session.createQuery("from User", User.class);
+            return getAllUsers.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t get all users from db", e);
+        }
+    }
+}
