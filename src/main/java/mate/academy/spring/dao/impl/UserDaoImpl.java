de594984a55300993f8e66224102f304bae573c6
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-12 08:46:42 +0200

    implement UserServiceImpl

--- src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
@@ -6,12 +6,16 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class UserDaoImpl implements UserDao {
     private SessionFactory sessionFactory;
 
+    @Autowired
     public UserDaoImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
