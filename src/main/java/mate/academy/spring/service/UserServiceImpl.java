4901155b4a156c36dae0fb918436e6b2c6d34ae1
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-12 18:36:36 +0300

    make main()

--- src/main/java/mate/academy/spring/service/UserServiceImpl.java
+++ src/main/java/mate/academy/spring/service/UserServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
-    UserDao userDao;
+    private UserDao userDao;
 
     @Override
     public User add(User user) {
