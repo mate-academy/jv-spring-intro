7ffa240fed0d4d1b67b70a4f6cd4adc7abcb57c7
Vasyl Pankiv <wasilius2@gmail.com>
2022-10-14 13:54:18 +0300

    done spring intro

--- src/main/java/mate/academy/spring/config/AppConfig.java
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -1,5 +1,7 @@
 package mate.academy.spring.config;
 
+import java.util.Properties;
+import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +11,10 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
-import javax.sql.DataSource;
-import java.util.Properties;
-
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "mate.academy.spring")
-public class TestAppConfig {
+public class AppConfig {
     @Autowired
     private Environment environment;
 
@@ -33,14 +32,11 @@ public class TestAppConfig {
     public LocalSessionFactoryBean getSessionFactory() {
         LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
         localSessionFactoryBean.setDataSource(getDataSource());
-
         Properties properties = new Properties();
         properties.put("hibernate.show-sql", environment.getProperty("hibernate.show-sql"));
         properties.put("hibernate.hbm2ddl.auto",
                 environment.getProperty("hibernate.hbm2ddl.auto"));
-        properties.put("hibernate.dialect",
-                environment.getProperty("hibernate.dialect"));
-
+        properties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
         localSessionFactoryBean.setHibernateProperties(properties);
         localSessionFactoryBean.setPackagesToScan("mate.academy.spring.model");
         return localSessionFactoryBean;
