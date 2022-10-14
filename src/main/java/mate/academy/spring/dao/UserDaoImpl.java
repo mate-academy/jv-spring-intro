b40455f190fb2a3ea92d625ce78ac08344e74275
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-14 16:25:12 +0300

    UserDaoImpl was fixed

--- src/main/java/mate/academy/spring/dao/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/UserDaoImpl.java
@@ -2,7 +2,10 @@ package mate.academy.spring.dao;
 
 import java.util.List;
 import mate.academy.spring.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -17,11 +20,33 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User add(User user) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save user to DB, user=" + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return user;
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<User> query = session.createQuery("from User", User.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get users from DB", e);
+        }
     }
 }
--- src/main/java/mate/academy/spring/service/UserServiceImpl.java
+++ src/main/java/mate/academy/spring/service/UserServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-
     @Autowired
     private UserDao userDao;
 
