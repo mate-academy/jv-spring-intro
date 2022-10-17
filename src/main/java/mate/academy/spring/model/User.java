0c3dc367cfb171927e19c4fd966515d757c10a9f
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-17 14:14:03 +0300

    added empty space before User

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -9,7 +9,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class  User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
