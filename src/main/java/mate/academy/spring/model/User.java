d80c7a6448338d5dd099a884c967694e87893372
Anatolii Bespalko <tolyan9901@gmail.com>
2022-12-05 21:37:39 +0200

    My spring intro

---
+++ src/main/java/mate/academy/spring/model/User.java
@@ -0,0 +1,23 @@
+package mate.academy.spring.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@EqualsAndHashCode
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer age;
+    private String name;
+}
