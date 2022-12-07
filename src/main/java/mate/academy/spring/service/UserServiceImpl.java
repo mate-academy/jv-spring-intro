f354a3976dc702ddf563456f645ebce6fcef3e0f
Oleksandr Kravchenko <Pokemonishche@me.com>
2022-12-07 21:57:15 +0200

    hw done

---
+++ src/main/java/mate/academy/spring/service/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package mate.academy.spring.service;
+
+import java.util.List;
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public User add(User user) {
+        userDao.add(user);
+        return user;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+}
