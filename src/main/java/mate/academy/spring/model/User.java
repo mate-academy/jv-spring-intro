d650aed5921c2a3b278cfa32eb7c0d8671e47dac
Alex Anofriev <alex.linkoln7391@gmail.com>
2022-12-12 03:38:19 +0200

    Spring User model implementation

---
+++ src/main/java/mate/academy/spring/model/User.java
@@ -0,0 +1,27 @@
+package mate.academy.spring.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer age;
+    private String name;
+}
