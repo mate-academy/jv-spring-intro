08bc22e4911c737b03ae1a02306ecd977c6d27f5
Bohdan Senyk <senyk2001@gmail.com>
2022-12-13 13:30:10 +0200

    created solution for the task

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,19 @@
 package mate.academy.spring;
 
+import mate.academy.spring.context.ApplicationContext;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ApplicationContext.class);
+        UserService userService = context.getBean(UserService.class);
+        User user1 = new User();
+        user1.setAge(21);
+        user1.setName("Bohdan");
+        userService.add(user1);
+        System.out.println(userService.getAll());
     }
 }
