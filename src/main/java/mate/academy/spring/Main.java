6d0e90c78a3f1307e5b0867890ff20293e0d1628
Iryna Chernikova <i.chernikova14@ukr.net>
2022-12-13 22:38:16 +0200

    implemented task

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,19 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-
+        User user = new User();
+        user.setAge(21);
+        user.setName("Bob");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        System.out.println(userService.add(user));
+        System.out.println(userService.getAll());
     }
 }
