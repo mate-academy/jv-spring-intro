b0b9b6ff6da2c0c449b790c7e045d03505038960
Luka Lozovyk <lukalozovyk@gmail.com>
2022-12-13 21:14:44 +0200

    spring configured

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,26 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        User userLuka = new User();
+        userLuka.setAge(20);
+        userLuka.setName("Luka");
 
+        User userAlice = new User();
+        userAlice.setAge(15);
+        userAlice.setName("Alice");
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        System.out.println("First added user: " + userService.add(userLuka));
+        System.out.println("Second added user: " + userService.add(userAlice));
+
+        System.out.println(userService.getAll());
     }
 }
