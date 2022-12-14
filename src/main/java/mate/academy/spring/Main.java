82b04fc8975a4a31bfcbaf5c339133c41b9491a1
Serhii Prykhodko <sergii.prykhodko@gmail.com>
2022-12-14 20:47:55 +0200

    mvn fix

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -7,15 +7,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        UserService userService = context.getBean(UserService.class);
         User old = new User();
         old.setAge(40);
         old.setName("OldNoname");
         User yang = new User();
         yang.setAge(15);
         yang.setName("Noname");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
         System.out.println("Yang:" + userService.add(yang));
         System.out.println("Old: " + userService.add(old));
         System.out.println(userService.getAll());
