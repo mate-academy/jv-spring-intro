c3c000472996eb4a8d5a9d38cc45ff566d3f1f5d
Fokin <Fokin1@ukr.net>
2023-01-09 22:02:01 +0100

    create UserServiceImpl, UserDaoImpl, AppConfig

---
+++ src/main/java/mate/academy/spring/model/User.java
@@ -0,0 +1,59 @@
+package mate.academy.spring.model;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object inputUser) {
+        if (this == inputUser) {
+            return true;
+        }
+        if (inputUser == null || getClass() != inputUser.getClass()) {
+            return false;
+        }
+        User user = (User) inputUser;
+        return age == user.age && id.equals(user.id) && name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+}
