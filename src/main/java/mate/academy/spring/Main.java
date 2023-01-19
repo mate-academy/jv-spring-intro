03bd36bc5f55c204f1966e2ca608e70be6c82acd
AndriySichniy <sichniy.andriy@gmail.com>
2023-01-19 18:27:45 +0200

    appconfig and pom

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,24 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
 
+        List<User> users = new ArrayList<>();
+        users.add(new User("Bob", 30));
+        users.add(new User("Alice", 25));
+        users.add(new User("John", 20));
+        users.forEach(userService::add);
+        System.out.println(userService.getAll());
     }
 }
