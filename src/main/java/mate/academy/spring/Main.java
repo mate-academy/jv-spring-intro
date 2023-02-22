d006120d60ac476bd474009b6934e148207adbbf
Maksym Tseniukh <117209668+MaksymTseniukh@users.noreply.github.com>
2023-02-22 15:08:54 +0200

    1

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,19 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+    private static final UserService userService = context.getBean(UserService.class);
+
+    public static void main(String[] args) {
+        User user = new User("Max", 21);
+        userService.add(user);
+
+        System.out.println(userService.getAll());
     }
 }
