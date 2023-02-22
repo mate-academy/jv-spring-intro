f60ec944aa3e6fb8c4183cee1689e003f66a453b
Maksym Tseniukh <117209668+MaksymTseniukh@users.noreply.github.com>
2023-02-22 12:59:25 +0200

    First start

---
+++ src/main/java/mate/academy/spring/model/User.java
@@ -0,0 +1,61 @@
+package mate.academy.spring.model;
+
+import jakarta.persistence.Table;
+import java.util.Objects;
+
+@Table(name = "user")
+public class User {
+
+    private long id;
+    private int age;
+    private String  name;
+
+    public User(){
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, age, name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "id=" + id
+                + ", age=" + age
+                + ", name='" + name + '\''
+                +'}';
+    }
+}
