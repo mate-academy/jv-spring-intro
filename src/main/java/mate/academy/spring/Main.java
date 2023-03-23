0995b5aca2fcc5b9f457c73902a9830c0756ec5d
Olexii Melnik <alexmelnikk@gmail.com>
2023-03-23 21:39:21 +0200

    f1st commit

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,31 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
 
+        User userOlexii = new User();
+        userOlexii.setAge(15);
+        userOlexii.setName("Olexii");
+
+        User userViktoria = new User();
+        userViktoria.setAge(12);
+        userViktoria.setName("Viktoria");
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(userOlexii);
+        userService.add(userViktoria);
+
+        System.out.println(userService.getAll());
+
     }
+
 }
