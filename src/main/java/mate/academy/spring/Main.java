b1786fdadb0d6fe3d4458577eb4c29a827e3a22e
Dmytro Busyhin <zza4etik@gmail.com>
2023-04-01 23:00:24 +0300

    added user and corresponding dao, and service. Configured spring.

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,21 @@
 package mate.academy.spring;
 
+import java.util.List;
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService service = context.getBean(UserService.class);
+        User bob = new User();
+        bob.setName("Bob");
+        bob.setAge(30);
+        service.add(bob);
+        List<User> all = service.getAll();
+        System.out.println(all.size());
     }
 }
