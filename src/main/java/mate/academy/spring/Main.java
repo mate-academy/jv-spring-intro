57905051163d87afa8dfc99798abdfe0d37d17bf
kromchk <kromchyk@yahoo.com>
2023-04-08 08:07:00 +0300

    realization spring-intro

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,16 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-
+        AnnotationConfigApplicationContext ctx
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = ctx.getBean(UserService.class);
+        userService.add(new User());
+        userService.getAll();
     }
 }
