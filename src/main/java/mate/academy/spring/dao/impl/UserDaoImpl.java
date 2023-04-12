831ec78ceef09b1bfaba78c6cdf85ac86d272707
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-04-12 21:49:26 +0300

    fix

--- src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
@@ -42,7 +42,7 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public List<User> getAll() {
-        try (Session session = sessionFactory.openSession()){
+        try (Session session = sessionFactory.openSession()) {
             return session.createQuery("from User", User.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Can't get all users from db", e);
