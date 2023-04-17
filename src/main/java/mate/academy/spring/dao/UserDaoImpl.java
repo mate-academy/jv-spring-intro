7d7edead7fdf9cf80ba2327d44015e8a2505335e
Serhii Shyian <serhii.shn@gmail.com>
2023-04-17 17:48:06 +0300

    1) Added solution

---
+++ src/main/java/mate/academy/spring/dao/UserDaoImpl.java
@@ -0,0 +1,55 @@
+package mate.academy.spring.dao;
+
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import mate.academy.spring.exception.DataProcessingException;
+import mate.academy.spring.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public User add(User user) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert a user " + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaQuery<User> getAllUsersQuery = session.getCriteriaBuilder()
+                    .createQuery(User.class);
+            getAllUsersQuery.from(User.class);
+            return session.createQuery(getAllUsersQuery).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get all users", e);
+        }
+    }
+}
