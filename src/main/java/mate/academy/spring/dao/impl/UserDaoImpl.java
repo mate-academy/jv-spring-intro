1749668cf193f7532bce05424b616289ad1946c3
Skrudj <ssinilov@gmail.com>
2023-04-18 17:52:40 +0300

    create initial app structure

---
+++ src/main/java/mate/academy/spring/dao/impl/UserDaoImpl.java
@@ -0,0 +1,17 @@
+package mate.academy.spring.dao.impl;
+
+import java.util.List;
+import mate.academy.spring.dao.UserDao;
+import mate.academy.spring.model.User;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+}
---
+++ src/main/java/mate/academy/spring/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package mate.academy.spring.service.impl;
+
+import java.util.List;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User add(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+}
