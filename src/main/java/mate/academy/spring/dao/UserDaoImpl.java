815df6a5cc5f41598e86f2c903492dc59d0d4d06
Alona Klishchenko <alyonaklischenko@gmail.com>
2023-04-24 23:40:44 +0300

    made first spring app

---
+++ src/main/java/mate/academy/spring/dao/UserDaoImpl.java
@@ -0,0 +1,53 @@
+package mate.academy.spring.dao;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import mate.academy.spring.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add user " + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            List<User> userList = session.createQuery("SELECT u FROM User u ",
+                    User.class).getResultList();
+            return userList;
+        } catch (Exception e) {
+            throw new NoSuchElementException("Can't get users ", e);
+        }
+    }
+}
