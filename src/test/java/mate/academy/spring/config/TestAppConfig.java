1120d66595ae4ca7b223bca22fd830245bdeafcd
Denys <denchik9026@gmail.com>
2023-04-25 13:22:29 +0300

    again changing properties

--- src/test/java/mate/academy/spring/config/TestAppConfig.java
+++ src/test/java/mate/academy/spring/config/TestAppConfig.java
@@ -35,9 +35,9 @@ public class TestAppConfig {
         localSessionFactoryBean.setDataSource(getDataSource());
 
         Properties properties = new Properties();
-        properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.put("show_sql", environment.getProperty("hibernate.show_sql"));
         properties.put("hibernate.hbm2ddl.auto",
-                environment.getProperty("hibernate.hbm2ddl.auto"));
+                environment.getProperty("hibernate.hbm2_ddl.auto"));
         properties.put("hibernate.dialect",
                 environment.getProperty("hibernate.dialect"));
 
