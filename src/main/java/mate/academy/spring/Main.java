6189a3be43e20c1bd7db72c7551aeeda3ee08a57
Denys <denchik9026@gmail.com>
2023-04-25 12:29:48 +0300

    configure Spring and write simple User service

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,21 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+    private static final UserService userService = context.getBean(UserService.class);
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("Bob");
+        user.setAge(22);
+
+        userService.add(user);
+        userService.getAll().forEach(System.out::println);
     }
 }
