b54e7301e5505cc9975144df3079b9e76e724351
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2023-05-01 02:54:07 +0300

    Initial commit: all requirements were implemented.

---
+++ src/main/java/mate/academy/spring/model/User.java
@@ -0,0 +1,64 @@
+package mate.academy.spring.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer age;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, id, name);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        User user = (User) obj;
+        return Objects.equals(age, user.age) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", age=" + age + ", name=" + name + "]";
+    }
+}
