245b9b34aa60809a399e7a6522839db97fe2c019
Sofiia <sofiakazzz@gmail.com>
2023-05-08 11:04:05 +0300

    Was added needed dependency and implemented UserService and UserDao.

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,21 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
     public static void main(String[] args) {
 
+        UserService userService = context.getBean(UserService.class);
+        User bob = new User();
+        bob.setName("Bob");
+        bob.setAge(20);
+        userService.add(bob);
+        System.out.println(userService.getAll());
     }
 }
