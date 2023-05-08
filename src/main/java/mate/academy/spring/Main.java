7ae680ce323f3c4a7415e60fc88f2778794ee2f1
dovy_al@ukr.net <dovy_al@ukr.net>
2023-05-08 14:20:05 +0300

    hw solutions

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,23 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+    public static final int AGE = 29;
+    public static final String NAME = "Bob";
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+        User user = new User();
+        user.setAge(AGE);
+        user.setName(NAME);
+        System.out.println(userService.add(user));
+        System.out.println(userService.getAll());
     }
 }
