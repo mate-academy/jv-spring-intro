51db118604a8f41f7c99b9d0b98d15d2fde5fe85
iskamele <iskamele@gmail.com>
2023-05-22 22:30:52 +0300

    Implemented User model, service and dao for it.

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,31 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        // create users
+        User bob = new User();
+        bob.setName("Bob");
+        bob.setAge(25);
+
+        User alise = new User();
+        alise.setName("Alise");
+        alise.setAge(20);
+
+        // add users
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(bob);
+        userService.add(alise);
+
+        // print users
+        System.out.println(userService.getAll());
     }
 }
