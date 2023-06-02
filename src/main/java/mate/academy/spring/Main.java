43234def37b2eee835b383c2ab073d6c83f1bb0c
Valerii <kulibchenkovalera@gmail.com>
2023-06-02 15:39:23 +0300

    Firts commit

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,25 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        User user = new User();
+        user.setName("Alice");
+        user.setAge(19);
+        User user2 = new User();
+        user2.setName("Bob");
+        user2.setAge(25);
 
+        AnnotationConfigApplicationContext context = new
+                AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+        userService.add(user);
+        userService.add(user2);
+
+        System.out.println(userService.getAll());
     }
 }
