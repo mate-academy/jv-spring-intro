d7a7e4336d131417ba1b0d2dee550f1a13e60d76
aTiukhtii <130995423+aTiukhtii@users.noreply.github.com>
2023-07-03 01:26:06 +0300

    Update AppConfig.java

--- src/main/java/mate/academy/spring/config/AppConfig.java
+++ src/main/java/mate/academy/spring/config/AppConfig.java
@@ -35,7 +35,8 @@
 //         Properties properties = new Properties();
 //         properties.put("show_sql", environment
 //                        .getProperty("hibernate.show_sql"));
-//         properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+//         properties.put("hibernate.hbm2ddl.auto", environment
+//.getProperty("hibernate.hbm2ddl.auto"));
 //         localSessionFactoryBean.setHibernateProperties(properties);
 //         localSessionFactoryBean.setPackagesToScan("mate.academy.spring.model");
 //         return localSessionFactoryBean;
