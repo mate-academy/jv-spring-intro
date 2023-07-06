79abb54c8ec4988a973eb68d5d476e0a1869eaba
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-07-06 18:11:32 +0300

    built according readme file

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,30 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = applicationContext.getBean(UserService.class);
+
+        User userBob = new User();
+        userBob.setName("Bob");
+        userBob.setAge(327);
+
+        User userJorge = new User();
+        userJorge.setName("Jorge");
+        userJorge.setAge(13);
+
+        userService.add(userBob);
+        userService.add(userJorge);
+
+        List<User> allUsers = userService.getAll();
+
 
     }
 }
