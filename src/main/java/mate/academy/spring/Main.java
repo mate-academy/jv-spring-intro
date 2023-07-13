209b27575faad08066558039f37a3d59c4ff4545
Viktoriia <reraitviktoria@gmail.com>
2023-07-13 12:47:23 +0200

    HW-Spring-13OfJuly

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,22 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
 
+        UserService userService = context.getBean(UserService.class);
+        User bob = new User(25, "Bob");
+        userService.add(bob);
+
+        User i = new User(25, "I");
+        userService.add(i);
+
+        System.out.println(userService.getAll());
     }
 }
