e88818f1ae17eeec92835e5670f482cc7c27b97b
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-07-13 18:47:25 +0300

    spring-intro-first-version

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,22 @@
 package mate.academy.spring;
 
+import java.util.List;
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
+        User bob = new User(22, "Bob");
+        User vlad = new User(28, "Vlad");
+        UserService userService = context.getBean(UserService.class);
+        userService.add(bob);
+        userService.add(vlad);
+        List<User> allUsers = userService.getAll();
+        allUsers.forEach(System.out::println);
     }
 }
