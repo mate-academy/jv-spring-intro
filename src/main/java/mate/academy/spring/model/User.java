bd5a19200fba9e4a144fb068327788ffac96e8cf
Gleb Chebotarov <taronassvolt@gmail.com>
2023-07-15 12:10:45 +0300

    done

--- src/main/java/mate/academy/spring/model/User.java
+++ src/main/java/mate/academy/spring/model/User.java
@@ -28,6 +28,14 @@ public class User {
         this.name = name;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public int getAge() {
         return age;
     }
