c902129f7a0e4d2ef3af77dfd9b7a4ca5000d213
Gleb Chebotarov <taronassvolt@gmail.com>
2023-07-15 11:58:19 +0300

    test

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,22 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context
+                = new AnnotationConfigApplicationContext(AppConfig.class);
 
+        UserService userService = context.getBean(UserService.class);
+        User tom = new User(25,"Tom");
+        userService.add(tom);
+
+        User anna = new User(18, "Anna");
+        userService.add(anna);
+
+        System.out.println(userService.getAll());
     }
 }
