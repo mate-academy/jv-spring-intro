3d6cfd84d0e93ade25a1068d360ffd7db9726bd5
DVArt <dvart@hotmail.com>
2023-07-17 19:31:42 +0300

    spring_intro

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,22 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+    private static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        UserService userService = context.getBean(UserService.class);
+        User testUser = new User();
+        testUser.setName("TestUser");
+        testUser.setAge(18);
+
+        userService.add(testUser);
+
+        userService.getAll().forEach(System.out::println);
     }
 }
