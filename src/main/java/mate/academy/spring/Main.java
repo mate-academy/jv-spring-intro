f95667585e04f01d9be7acf6010c36a503add684
Victor Ruban <edmont373@gmail.com>
2023-07-18 09:19:38 +0300

    fix problem

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,20 @@
 package mate.academy.spring;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+    public static final AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    public static void main(String[] args) {
+        User mike = new User();
+        mike.setAge(24);
+        mike.setName("Me");
+        UserService userService = context.getBean(UserService.class);
+        userService.add(mike);
+        userService.getAll().forEach(System.out::println);
     }
 }
