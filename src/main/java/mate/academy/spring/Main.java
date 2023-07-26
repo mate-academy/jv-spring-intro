94a3355bc29417b12cc60f8fa2e2aa0d4740ed86
Ilya Samilyak <samilyakilya@gmail.com>
2023-07-26 20:22:01 +0300

    HW-spring-intro

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,23 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        User ilya = new User(40, "Ilya");
+        User mila = new User(14, "Mila");
+        userService.add(ilya);
+        userService.add(mila);
+
+        System.out.println(userService.getAll());
 
     }
 }
