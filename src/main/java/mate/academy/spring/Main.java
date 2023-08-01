b06bf6fecd50b58f6bcb7107065383e7966e11e6
Artem Kostenko <a_kostenco@yahoo.com>
2023-08-01 20:21:33 +0300

    spring intro solution first commit

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -1,7 +1,28 @@
 package mate.academy.spring;
 
+import mate.academy.spring.config.AppConfig;
+import mate.academy.spring.model.User;
+import mate.academy.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+
+        User userBob = new User();
+        userBob.setName("Bob");
+        userBob.setAge(25);
+
+        User userAlice = new User();
+        userAlice.setName("Alice");
+        userAlice.setAge(29);
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(userBob);
+        userService.add(userAlice);
+        System.out.println(userService.getAll());
 
     }
 }
